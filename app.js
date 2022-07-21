653d37a8da9d6a6bde78b101515eeabebbab5b69
Lakshmi_Vara_Prasad Kuna <klvpfeb98@gmail.com>
2022-07-21 12:32:41 +0530

    modified on cors

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 // DB connection
 const MONGO_URL =
-  "mongodb://localhost:27017/authentication" || process.env.DB_URL;
+  process.env.DB_URL || "mongodb://localhost:27017/authentication";
 
 mongoose
   .connect(MONGO_URL, {
